324b31f9c33f82172f90a567c328e4382a1935b3
weimerq <150670107+weimerq@users.noreply.github.com>
2023-12-10 13:18:29 +0100

    Add files via upload

---
+++ images/Sanrio Landingpage_files/script.js
@@ -0,0 +1,8 @@
+var logo = document.querySelector('.hamburger');
+var menu = document.querySelector('.menu');
+
+logo.addEventListener('click',function(){
+    menu.classList.toggle("showmenu");
+})
+
+s
